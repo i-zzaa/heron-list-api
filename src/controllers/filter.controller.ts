2bf05b944b0cee16001a91c767f29f2747154cd3
Andressa Novaes <isa.desenvolvimento@gmail.com>
2023-02-05 21:33:33 -0300

    fix: dropdown

--- src/controllers/filter.controller.ts
+++ src/controllers/filter.controller.ts
@@ -114,7 +114,7 @@ export class filterController {
             },
             where: {
               NOT: {
-                nome: 'Terapia ABA',
+                nome: 'Terapia',
               },
             },
           });
